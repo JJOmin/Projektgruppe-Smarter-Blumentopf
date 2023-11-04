9abf991cfd276065813dfdd17560a0e79725dd06
JJOmin <104137706+JJOmin@users.noreply.github.com>
2023-11-04 18:45:20 +0100

    Testing

---
+++ ESP32-Code/Code/LED IsESPstillRunning/main.py
@@ -0,0 +1,19 @@
+import machine
+import time
+
+# Definiere die Pins für die LED (hier: Pin 2) und den Button (hier: Pin 4)
+led_pin = machine.Pin(25, machine.Pin.OUT)
+button_pin = machine.Pin(34, machine.Pin.IN, machine.Pin.PULL_UP)  # PULL_UP aktiviert den internen Pull-Up Widerstand
+#led_pin.off()
+
+# Heartbeat-Funktion mit Button-Erkennung
+def heartbeat_with_button(led_pin, button_pin):
+    while True:
+        # Wenn der Button gedrückt wird (niedriger Pegel), schalte die LED ein
+        if not button_pin.value():
+            led_pin.on()
+        else:
+            led_pin.off()
+
+# Rufe die Heartbeat-Funktion mit Button-Erkennung auf
+heartbeat_with_button(led_pin, button_pin)
