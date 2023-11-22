0964000ef6d677e6c7ba11e1776364876d95200b
JJOmin <104137706+JJOmin@users.noreply.github.com>
2023-11-22 11:39:06 +0100

    Jetzt URL etwas schöner

--- ESP32-Code/CodeFuerESP/ServerConnection/main.py
+++ ESP32-Code/CodeFuerESP/ServerConnection/main.py
@@ -78,8 +78,8 @@ class ServerConnection:
 # Informationen die benötigt werden um die Klasse zu initialisieren
 ssid = 'Kleiner Riese Oben'
 password = 'Hola_Emden_Wifi'
-remote_url = 'https://cloudleo.duckdns.org/Blumentopf/Database/daten.json'
-upload_url = 'http://31.19.90.130:5000/Blumentopf/upload_data'  # Sollte noch in die Offizielle URL geändert werden, aber aktuell noch nicht freigegeben
+remote_url = 'https://www.tilly.cloud/Blumentopf/Database/daten.json'  # falls fehler auftrete auf "https://cloudleo.duckdns.org/Blumentopf/Database/daten.json" setzen
+upload_url = 'https://blumentopfupload.tilly.cloud/Blumentopf/upload_data'  # falls fehler auftreten auf "http://31.19.90.130:5000/Blumentopf/upload_data" setzen
 username = b'Blumentopf'
 password_b = b'Blumentopf_123'
 
