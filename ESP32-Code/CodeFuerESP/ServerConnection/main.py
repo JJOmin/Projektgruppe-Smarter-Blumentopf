95755ff882ca5b4db3e4595242872f032f71e3f6
JJOmin <104137706+JJOmin@users.noreply.github.com>
2023-11-22 11:18:18 +0100

    Bisl Schöner

--- ESP32-Code/CodeFuerESP/ServerConnection/main.py
+++ ESP32-Code/CodeFuerESP/ServerConnection/main.py
@@ -8,82 +8,84 @@ import requests
 from machine import RTC
 
 
-
-print("Connecting to WiFi", end="")
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('Kleiner Riese Oben', 'Hola_Emden_Wifi')
-while not sta_if.isconnected():
-    print(".", end="")
-    time.sleep(0.1)
-print(" Connected!")
-
-# URL zur Datei auf dem entfernten Webserver
-url = 'https://cloudleo.duckdns.org/Blumentopf/Database/daten.json'
-url2 = 'http://31.19.90.130:5000/Blumentopf/upload_data' #Sollte noch in die Offizielle URL geändert werden, aber aktuell noch nicht freigegeben
-
-# Benutzername und Passwort für die Basic-Authentifizierung
-username = b'Blumentopf'
-password = b'Blumentopf_123'
-
-# Funktion, um Daten von der entfernten Datei abzurufen
-def get_remote_file_content(url):
-    auth = 'Basic ' + ubinascii.b2a_base64(username + b":" + password).strip().decode('utf-8')
-    headers = {'Authorization': auth}
-    
-    response = urequests.get(url, headers=headers)
-    if response.status_code == 200:
-        content = response.text
-        response.close()
-        return content
-    else:
-        response.close()
-        return None
-    
-def currentTime():
-    rtc = RTC()
-    datetime = rtc.datetime()
-    # Informationen extrahieren
-    year = datetime[0]
-    month = datetime[1]
-    day = datetime[2]
-    weekday = datetime[3]  # Wochentag (0 = Montag, 6 = Sonntag)
-    hour = datetime[4]
-    minute = datetime[5]
-    second = datetime[6]
-    array = []
-    array.append(["{0:04d}-{1:02d}-{2:02d}".format(year, month, day)])
-    array.append(["{0:02d}:{1:02d}:{2:02d}".format(hour, minute, second)])
-    return array
-    
-def set_Value_Server(url2):
-    # Beispiel-Daten als JSON
-    print(currentTime()[0])
-    data = {
-        "Datum": currentTime()[0],
-        "Uhrzeit": currentTime()[1]
-    }
-    # Senden der POST-Anfrage
-    response = requests.post(url2, json=data)
-
-    # Überprüfen der Antwort
-    if response.status_code == 200:
-        return("Daten erfolgreich gesendet und gespeichert.")
-    else:
-        return("Fehler beim Senden der Daten:", response.status_code)
-    
-
-# Hauptprogramm
-def main():
-    print(set_Value_Server(url2))
-    file_content = get_remote_file_content(url)
-    if file_content:
-        print('Inhalt der Datei aus', url)
-        print("")
-        print("Ausgabe als convertiertes Array: ")
-        print(json.loads(file_content)) #Konvertierung Json in Python array
-        #print(json.loads(file_content).keys())
-    else:
-        print('Fehler beim Abrufen der Datei.')
+class ServerConnection:
+    def __init__(self, ssid, password, remote_url, upload_url, username, password_b):
+        self.ssid = ssid
+        self.password = password
+        self.remote_url = remote_url
+        self.upload_url = upload_url
+        self.username = username
+        self.password_b = password_b
+        self.sta_if = network.WLAN(network.STA_IF)
         
-main()
+    def connect_to_wifi(self):
+        print("Connecting to WiFi", end="")
+        self.sta_if.active(True)
+        self.sta_if.connect(self.ssid, self.password)
+        while not self.sta_if.isconnected():
+            print(".", end="")
+            time.sleep(0.1)
+        print(" Connected!")
+    
+    def get_remote_file_content(self):
+        auth = 'Basic ' + ubinascii.b2a_base64(self.username + b":" + self.password_b).strip().decode('utf-8')
+        headers = {'Authorization': auth}
+        
+        response = urequests.get(self.remote_url, headers=headers)
+        if response.status_code == 200:
+            content = response.text
+            response.close()
+            return content
+        else:
+            response.close()
+            return None
+    
+    def get_date(self):
+        rtc = RTC()
+        datetime = rtc.datetime()
+        year, month, day, _, hour, minute, second, _ = datetime
+        date_str = "{0:04d}-{1:02d}-{2:02d}".format(year, month, day)
+        time_str = "{0:02d}:{1:02d}:{2:02d}".format(hour, minute, second)
+        return [date_str, time_str]
+    
+    def set_readings_to_server(self):
+        date_time = self.get_date()
+        data = {
+            "Datum": date_time[0],
+            "Uhrzeit": date_time[1],
+            
+        }
+        response = requests.post(self.upload_url, json=data)
+        if response.status_code == 200:
+            return "Daten erfolgreich gesendet und gespeichert."
+        else:
+            return f"Fehler beim Senden der Daten: {response.status_code}"
+    
+#     def main(self):
+#         self.connect_to_wifi()
+#         print(self.set_readings_to_server())
+#         file_content = self.get_remote_file_content()
+#         if file_content:
+#             
+#             #print('Inhalt der Datei aus', self.remote_url)
+#             #print("")
+#             #print("Ausgabe als convertiertes Array: ")
+#             #print(json.loads(file_content))  # Konvertierung Json in Python-Array
+#         else:
+#             print('Fehler beim Abrufen der Datei.')
+
+
+# Informationen die benötigt werden um die Klasse zu initialisieren
+ssid = 'Kleiner Riese Oben'
+password = 'Hola_Emden_Wifi'
+remote_url = 'https://cloudleo.duckdns.org/Blumentopf/Database/daten.json'
+upload_url = 'http://31.19.90.130:5000/Blumentopf/upload_data'  # Sollte noch in die Offizielle URL geändert werden, aber aktuell noch nicht freigegeben
+username = b'Blumentopf'
+password_b = b'Blumentopf_123'
+
+connection = ServerConnection(ssid, password, remote_url, upload_url, username, password_b)
+connection.connect_to_wifi()
+print(connection.get_remote_file_content())
+print(connection.set_readings_to_server())
+print(connection.get_remote_file_content())
+
