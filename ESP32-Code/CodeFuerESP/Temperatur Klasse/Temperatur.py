5e8277e311261c7cf497079bd8e74819b58dbb32
NisVison <122164022+NisVison@users.noreply.github.com>
2023-11-23 14:54:54 +0100

    update

--- ESP32-Code/CodeFuerESP/Temperatur Klasse/Temperatur.py
+++ ESP32-Code/CodeFuerESP/Temperatur Klasse/Temperatur.py
@@ -6,7 +6,7 @@ class Temperatur:
     def __init__(self, ds_pin, btn_pin): 
         self.ds_pin = machine.Pin(ds_pin) # pin für Sensor
         self.ds_sensor = ds18x20.DS18X20(onewire.OneWire(self.ds_pin)) # Kominikation mit Sensor
-        self.btnYellow = machine.Pin(btn_pin, machine.Pin.IN) # Taster(hier zufällig gelb)
+        self.btnColor = machine.Pin(btn_pin, machine.Pin.IN) # Taster
         self.roms = self.ds_sensor.scan() #Suche nach DS18B20-Sensoren
         print('Found DS devices: ', self.roms) #Ausgabe der gefundenen Adressen:
         self.running = False # Maybe muss das true sein(noch nicht sicher)
@@ -19,11 +19,11 @@ class Temperatur:
             print("Temperatur: {:.2f}°C".format(temperature)) #Ausgabe der Temperatur + Formatierung
 
     def start_by_press(self): # die schleife wird ausgeführt wenn der taster gedrückt wird
-        self.running = True # S
+        self.running = True # Start variable die abgefragt wird um start_by_press zu beenden.
         while self.running: # endloschschleife solange 
-            self.read_temperature()
-            if self.btnYellow.value() == 0: #
-                self.running = False
+            self.read_temperature() # Starte die methode read_temperatur() Also ließ
+            if self.btnColor.value() == 0: #Wenn sich der Wert vom knopf ändert
+                self.running = False # Setze die Prüf variable auf false damit die funktion start_by_press nicht mehr ausgeführt wird
                 break
             
             
@@ -32,5 +32,5 @@ class Temperatur:
 temperatur_sensor = Temperatur(ds_pin=22, btn_pin=34) #wichtig geht nur mit Pin22
 
 while True: # Endlosschleife die darauf wartet das Btn gedrückt wird und dann Methoden istanziert
-    if temperatur_sensor.btnYellow.value(): # wenn der wert vom btn True ist
-        temperatur_sensor.read_temperature() # startet diese Methode
+    if temperatur_sensor.btnColor.value(): # wenn der wert vom btn True ist
+        temperatur_sensor.read_temperature() # Startet diese Methode
