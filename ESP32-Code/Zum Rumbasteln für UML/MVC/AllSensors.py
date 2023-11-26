944ae1c6dabafc28f12295f0c4938edec877be11
JJOmin <104137706+JJOmin@users.noreply.github.com>
2023-11-26 19:21:14 +0100

    Temperature Sensor might work but i could not test it. @NisVison might get it tom work

--- ESP32-Code/Zum Rumbasteln für UML/MVC/AllSensors.py
+++ ESP32-Code/Zum Rumbasteln für UML/MVC/AllSensors.py
@@ -6,9 +6,11 @@ class AllSensors: #Class that holds instances of every sensor to get measurments
     def __init__(self, soilData):
         self.soilData = soilData
         soilSensor = SoilSensor(soilData)
-        self.soilSensorReading = 'no data'
-        self.lightSensorReading = 'no data'
-        self.temperaturSensorReading = 'no data'
         
-    def get():
-        return 
+        
+        self.soilSensorValue = 'no data'
+        self.lightSensorValue = 'no data'
+        self.temperaturSensorValue = 'no data'
+        
+    def readSoilSensor():
+        self.soilSensorValue = soilSensor.read_moisture()
