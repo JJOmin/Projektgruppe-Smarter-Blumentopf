8106f03a1ea60098bd3c99a05f00f70da6b0e580
Discovery1701A <klettern.rosine0v@icloud.com>
2023-12-02 15:13:37 +0100

    hab sleep aus der display klasse genommen und das der text egal wieviel komplett durch läuft

--- ESP32-Code/CodeFuerESP/Display/Display.py
+++ ESP32-Code/CodeFuerESP/Display/Display.py
@@ -3,12 +3,15 @@ from machine import Pin, SoftI2C
 from lcd_api import LcdApi
 from i2c_lcd import I2cLcd
 from time import sleep
+import utime
+
 
 class Display:
     def __init__(self):
         self.I2C_ADDR = 0x27
         self.totalRows = 2
         self.totalColumns = 16
+        self.last_measure_time = 0
 
         try:
             self.i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)  # I2C für ESP32
@@ -17,12 +20,32 @@ class Display:
         except Exception as e:
             print("Fehler beim Initialisieren des LCDs:", e)
 
-    def displayausgabe(self, text):
+    def displayausgabe(self, text, delay_ms=2000):
         try:
+            # Clear the LCD once before displaying any text
             self.lcd.clear()
-            self.lcd.putstr(str(text))
-            sleep(1)
+
+            # Zeichen pro Zeile
+            chars_per_line = self.totalColumns * self.totalRows
+
+            # Text in Teile aufteilen
+            text_parts = [text[i:i + chars_per_line] for i in range(0, len(text), chars_per_line)]
+            current_time = utime.ticks_ms()
+
+            for part in text_parts:
+                self.lcd.clear()
+                if current_time - self.last_measure_time >= delay_ms:
+                    # Only update the content without clearing the LCD
+                    self.lcd.move_to(0, 0)  # Move the cursor to the beginning
+                    self.lcd.putstr(part)
+                    self.last_measure_time = current_time
+
+                    # Pausiere für die angegebene Zeit in Millisekunden
+                    utime.sleep_ms(delay_ms)
+                    current_time = utime.ticks_ms()  # Aktualisiere die aktuelle Zeit
+
         except Exception as e:
             print("Fehler beim Schreiben auf das LCD:", e)
+
 
 
