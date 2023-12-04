3d1f19d695683c6f66c6cf026a854ac46dda74c1
Discovery1701A <klettern.rosine0v@icloud.com>
2023-12-04 09:52:37 +0100

    Update Display.py

--- ESP32-Code/CodeFuerESP/Display/Display.py
+++ ESP32-Code/CodeFuerESP/Display/Display.py
@@ -10,13 +10,11 @@ class Display:
     def __init__(self):
         self.I2C_ADDR = 0x27
         self.totalRows = 2
-    
-        self.last_measure_time = 0
         self.totalColumns = 16
+        self.current_display_text = ""  # Add this line
 
         try:
             self.i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)  # I2C für ESP32
-            # i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)  # I2C für ESP8266
             self.lcd = I2cLcd(self.i2c, self.I2C_ADDR, self.totalRows, self.totalColumns)
         except Exception as e:
             print("Fehler beim Initialisieren des LCDs:", e)
@@ -54,58 +52,56 @@ class Display:
             print("Fehler beim Schreiben auf das LCD:", e)
 
 
-    def display_centered_text(self, text, delay_ms=3000):
+    def display_centered_text(self, text, delay_ms=2000):
         try:
-            # Clear the LCD once before displaying any text
             self.lcd.clear()
 
-            # Zeichen pro Zeile
             chars_per_line = self.totalColumns
-
-            # Text in Teile aufteilen
             parts = text.split(':')
             lines = []
 
-            # Gruppieren von Schlüssel-Wert-Paaren
             for i in range(0, len(parts), 2):
                 key = parts[i].strip()
                 value = parts[i + 1].strip() if i + 1 < len(parts) else ""
                 lines.append(key)
-               # print(lines)
                 lines.append(value)
-                #print(lines)
 
-            # Zentriere jede Zeile und füge Leerzeichen für die Ausrichtung hinzu
             centered_lines = [(chars_per_line - len(line)) // 2 * ' ' + line + ' ' * ((chars_per_line - len(line)) // 2) for line in lines]
 
-           # print(centered_lines)
-            # Nur einmal clear() aufrufen, bevor die Schleife startet
             self.lcd.clear()
 
             current_time = utime.ticks_ms()
             idx = 0
 
-            while True:
-     
-                    
+            while idx < len(centered_lines):
                 if utime.ticks_ms() - current_time >= delay_ms:
-                    if idx% 2 ==0:
+                    if idx % 2 == 0:
                         self.lcd.clear()
-                    # Nur update die Inhalte, ohne das gesamte LCD zu löschen
-                   # self.lcd.move_to(idx%2, idx)  # Cursor an den Anfang setzen
                     self.lcd.putstr(centered_lines[idx])
-                    #print(centered_lines[idx])
-                    idx = (idx + 1) % len(centered_lines)  # Wechsel zur nächsten Zeile
-                    #print(idx)
-                    current_time = utime.ticks_ms()  # Aktualisiere die aktuelle Zeit
-                    #self.lcd.clear()
-    
+                    idx = (idx + 1) % len(centered_lines)
+                    current_time = utime.ticks_ms()
+                    print(idx)
+                    if idx == 0:
+                        break
+
         except Exception as e:
             print("Fehler beim Schreiben auf das LCD:", e)
 
 
-#display = Display()
-#while True:
-    #display_text = "Feuchtigkeit:100%:Temperatur:12:Licht:www"
-    #display.display_centered_text(display_text)
+    def update_display_text(self, new_text):
+        self.current_display_text = new_text
 
+    def display_updated_text(self):
+        self.display_centered_text(self.current_display_text)
+
+# Beispielaufruf
+#display = Display()
+#x = 0
+#current_time = utime.ticks_ms()
+
+#while True:
+    #print(x)
+    #display_text = "Feuchtigkeit:" + str(x) + "%:Temperatur:" + str(x) + ":Licht:" + str(x)+":"
+    #display.update_display_text(display_text)
+    #display.display_updated_text()
+    #x += 1      
