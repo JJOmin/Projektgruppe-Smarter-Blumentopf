3529be471d523c6721a0f1aea1bb7dc135d38473
NisVison <122164022+NisVison@users.noreply.github.com>
2023-12-06 18:20:45 +0100

    06.12.2023

--- 30.11 MVC und mehr code/main.py
+++ 30.11 MVC und mehr code/main.py
@@ -1,16 +1,19 @@
 #main
 from Control import Control
-
+from View import View
 
 control = Control()
+view = View()
 while True:
+    view.printAllData()
     control.startByPress()
-    control.sensorSoilTest()
+    #control.serverTest()
+    #control.sensorSoilTest()
     #control.serverTest()#Tests the server Connection (on the Hotspot of Leos Phone)
 
     
-    if control.model.btnData == 0:  # Annahme: btnData ist ein Integer-Wert
-        control.sensorTemperatureTest() # funktioniert
+    #if control.model.btnData == 0:  # Annahme: btnData ist ein Integer-Wert
+        #control.sensorTemperatureTest() # funktioniert
         #control.sensorLightTest()
          
 
